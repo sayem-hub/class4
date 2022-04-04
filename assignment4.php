7ff7ccf807522ac0a5b41f368a6971621f4e3e33
Abu Sayem Amir <76561416+sayem-hub@users.noreply.github.com>
2022-04-04 16:07:26 +0600

    making all days dropdown

---
+++ assignment4.php
@@ -0,0 +1,18 @@
+<?php 
+
+        /* Question 01:
+Your client said that he needs a dropdown, which contains all days of the running month. Sometimes we fill up a registration form that, which date of birth. So find out in google (get number of days in specific month php) and do this assignment using a for loop.
+Day quantity is not equal for every month. Like as - february - 28, March - 31, april - 30
+So days of running month should dynamic (by php build in function) */
+
+    $running_month = date("t");
+
+    // for ($running_month = 1; $running_month <31 ; $running_month++) { 
+    //     echo $running_month;
+    //     echo "<br>";
+    // }
+
+    
+echo    $running_month;
+
+?>
