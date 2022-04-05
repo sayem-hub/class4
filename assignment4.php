bc6a13ec33fa429060597aa1116f51c60c30d2d0
Abu Sayem Amir <76561416+sayem-hub@users.noreply.github.com>
2022-04-05 12:55:16 +0600

    completed all 5 questions

--- assignment4.php
+++ assignment4.php
@@ -1,9 +1,9 @@
 <?php 
 
                     /* Question 01:
-            Your client said that he needs a dropdown, which contains all days of the running month. Sometimes we fill up a registration form that, which date of birth. So find out in google (get number of days in specific month php) and do this assignment using a for loop.
-            Day quantity is not equal for every month. Like as - february - 28, March - 31, april - 30
-            So days of running month should dynamic (by php build in function) */
+                    Your client said that he needs a dropdown, which contains all days of the running month. Sometimes we fill up a registration form that, which date of birth. So find out in google (get number of days in specific month php) and do this assignment using a for loop.
+                    Day quantity is not equal for every month. Like as - february - 28, March - 31, april - 30
+                    So days of running month should dynamic (by php build in function) */
 
                     echo "<h3>Question-1: Output all days of running month</h3>"; //HTML Headline
                     echo "<hr/>"; //HTML horizontal line
@@ -16,7 +16,6 @@
                     echo $i;
                     echo "<br>";
                 }
-
                     /*Output
                     1
                     2
@@ -28,12 +27,12 @@
 
 
                     /* Question 02: 
-            If we print 1-100, our output will be 100 times. But this time you have print out only the odd (বিজোড়) number within 1-100.
-            You have to rotate from 1 to 100.
-            Your output message should like that, “3 is a odd number”
-            Not need to print even number */
+                    If we print 1-100, our output will be 100 times. But this time you have print out only the odd (বিজোড়) number within 1-100.
+                    You have to rotate from 1 to 100.
+                    Your output message should like that, “3 is a odd number”
+                    Not need to print even number */
 
-                    echo "<h3>Question-2: Odd number</h3>"; //HTML Headline
+                    echo "<h3>Question-2: Odd number within 1-100.</h3>"; //HTML Headline
                     echo "<hr/>"; //HTML horizontal line
 
 
@@ -42,8 +41,6 @@
                         echo $number . " ". "is a odd number";
                         echo "<br>";
                     }
-
-
                     /*Output 
                         1 is a odd number
                         3 is a odd number
@@ -56,17 +53,16 @@
 
 
                         /* Question 03: 
-            Factorial - I think everyone knows the word. Factorial of 4 is given below:
-            4! = 4*3*2*1 = 24
-            6! = 6*5*4*3*2*1 = 720  
-            So let’s calculate the factorial of 7. */
+                        Factorial - I think everyone knows the word. Factorial of 4 is given below:
+                        4! = 4*3*2*1 = 24
+                        6! = 6*5*4*3*2*1 = 720  
+                        So let’s calculate the factorial of 7. */
 
 
                     echo "<h3>Question-3: Factorial calculation of 7</h3>"; //HTML Headline
                     echo "<hr/>"; //HTML horizontal line
 
             //Main code here
-
                 $target_number = 7;
                 $factorial = 1;
 
@@ -75,20 +71,18 @@
                     }
 
                     echo "Factorial of 7! is = $factorial";
-
                     //Output: Factorial of 7! is = 5040
             
 
                     /* Question 04: 
-            Print out from 100 - 1 using a while loop. 
-            You must use decrement operator */
+                    Print out from 100 - 1 using a while loop. 
+                    You must use decrement operator */
 
                     echo "<h3>Question-4: Print out from 100 - 1 using a while loop</h3>"; //HTML Headline
                     echo "<hr/>"; //HTML horizontal line
 
 
             //Main code here
-
             $i = 100;
                     while ($i >= 1) {
                     echo $i;
@@ -102,4 +96,29 @@
                     97
                    ...
                     1 */
+
+
+                      /* Question 05: 
+                    You have an array of your purchased product items. 
+                    ['apple', 'orange', 'banana', 'mango']
+                    Show all products as a table list.
+                    Output should like that: “sl- 1 and product - apple” */
+
+                    echo "<h3>Question-5: Showing array products as table</h3>"; //HTML Headline
+                    echo "<hr/>"; //HTML horizontal line
+
+                //Main code here
+                $products = ['apple', 'orange', 'banana', 'mango'];
+                
+                    foreach ($products as $key => $value) {
+                       
+                        echo "SL-" . ++$key . ' ' . "Product - "  . $value;
+                        echo "<br>";
+                    }
+                    /*Output
+                    SL-1 Product - apple
+                    SL-2 Product - orange
+                    SL-3 Product - banana
+                    SL-4 Product - mango*/
+
 ?>
